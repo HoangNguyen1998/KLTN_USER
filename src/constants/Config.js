fa5d0e4ae2ade96490b64bf705d631efe78c8349
HoangNguyen1998 <nvh98.hv@gmail.com>
2020-04-25 11:45:00 +0700

    123

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,6 +1,6 @@
 // export const API_URL_USER = "http://jp-server-kltn.herokuapp.com/api/docs";
 // export const API_URL_USER = "http://jp-server-kltn.herokuapp.com/api/docs";
-// export const API_URL_USER = "https://learn-jp-kltn.herokuapp.com/api";
-export const API_URL_USER = "https://3401dac8.ngrok.io/api";
+export const API_URL_USER = "https://learn-jp-kltn.herokuapp.com/api";
+// export const API_URL_USER = "https://3401dac8.ngrok.io/api";
 
 export const TOKEN_SECRET = "TlhvN_k16";
--- src/reducers/Socket.js
+++ src/reducers/Socket.js
@@ -8,7 +8,7 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case SocketConstants.CONNECT_SOCKET: {
             console.log("Hello: ", getToken());
-            let socket = socketIOClient.connect("https://3401dac8.ngrok.io", {
+            let socket = socketIOClient.connect("https://learn-jp-kltn.herokuapp.com", {
                 query: "token=" + getToken(),
             });
             return {
